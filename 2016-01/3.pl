fd3445853c8df0b8537b50e92214465fb846a6d2
Rasmus Holm <rasho381@student.liu.se>
2018-08-20 16:00:47 +0200

    Add 3b

--- 2016-01/3.pl
+++ 2016-01/3.pl
@@ -1,3 +1,6 @@
+/*
+3a
+*/
 % add appended element to the end of the list
 append3([],ShiftedElement,ShiftedElement).
 append3([Head|InputList],ShiftedElement,[Head|OutputList]):-
@@ -5,3 +8,29 @@ append3([Head|InputList],ShiftedElement,[Head|OutputList]):-
 % lsh/2 left circular shift of a list, remove a element to the left, H, and append it to T
 lsh([H|T],O):-
     append3(T,[H],O).
+
+/*
+left shift is performed by using lsh([X1,...,Xn],Output).
+right shift is performed by using lsh(Output,[X1,...,Xn]).
+examples:
+Left shift
+lsh([1,2,3,4,5],O).
+Right shift
+lsh(O,[1,2,3,4,5]).
+*/
+
+/*
+3b
+input for arg 1 is on the form:
+[t1,..,tn|Z]-Z
+[t2,..,tn,t1|V]-V
+*/
+
+lshdl([X|List1]-Hole,O):-
+    append_dl(List1-Hole,[X|List2]-List2,O-[]).
+append_dl(I-M,M-O,I-O).
+
+/*
+examples:
+lshdl([1,2,3|X]-X,O).
+*/
